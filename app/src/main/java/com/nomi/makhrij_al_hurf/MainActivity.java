28c370891ba22b413e388f710c2a43c77cf0ea13
Nauman Aziz <m.nauman.aziz.1122@gmail.com>
2021-11-07 20:22:43 +0500

    all stable till link screen

--- app/src/main/java/com/nomi/makhrij_al_hurf/MainActivity.java
+++ app/src/main/java/com/nomi/makhrij_al_hurf/MainActivity.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView_;
+    Button btn_;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView_ = findViewById(R.id.repoPic);
+        imageView_ = findViewById(R.id.view5);
+        btn_ = findViewById(R.id.btn_login);
+
 
         imageView_.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +30,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btn_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
