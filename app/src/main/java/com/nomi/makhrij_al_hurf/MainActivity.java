cbc52280af4f86747eecb312e47aa976f346edb4
Nauman Aziz <m.nauman.aziz.1122@gmail.com>
2021-11-07 19:13:10 +0500

    theme set

--- app/src/main/java/com/nomi/makhrij_al_hurf/MainActivity.java
+++ app/src/main/java/com/nomi/makhrij_al_hurf/MainActivity.java
@@ -2,13 +2,29 @@ package com.nomi.makhrij_al_hurf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView_;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView_ = findViewById(R.id.repoPic);
+
+        imageView_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://casidiablo.net"));
+                startActivity(intent);
+            }
+        });
     }
 }
